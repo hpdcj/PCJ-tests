f610fa0b8be74c746c47b98f29264181556b2734
Marek Nowicki <faramir2@gmail.com>
2017-05-01 17:08:56 +0200

    Removed MPI tests from the repository.

--- mpi-src/moldyn/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. /vosoft/modules/Modules/3.2.8/init/bash
-module load mpich2
-mpicxx -lrt -lm -O2 -o mpich2_moldyn.exe moldyn.cpp
-srun /bin/hostname | sort > all_nodes.txt
-cat all_nodes.txt
-mpiexec -f=all_nodes.txt ./mpich2_moldyn.exe
--- mpi-src/raytracer/compile.sh
+++
@@ -1,6 +0,0 @@
-for i in openmpi mpich2 pgi
-do
-    module load $i
-    mpicxx -lm -lrt -O2 -o $i\_raytracer.exe raytracer.cpp
-    module unload $i 
-done
