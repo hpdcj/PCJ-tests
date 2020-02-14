b765de6b3a1d1807dd4ab102a0fdd11f1368febb
Marek Nowicki <faramir2@gmail.com>
2020-02-14 11:10:08 +0100

    DumpInfo only PcjThreads.

--- src/org/pcj/tests/DumpInfo.java
+++ src/org/pcj/tests/DumpInfo.java
@@ -5,28 +5,32 @@
  */
 package org.pcj.tests;
 
+import java.lang.management.LockInfo;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
+import java.lang.management.MonitorInfo;
+import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
+import java.util.Comparator;
 import java.util.concurrent.Executors;
 import java.util.function.Function;
 
 /**
- *
  * @author faramir
  */
 public class DumpInfo implements AutoCloseable {
 
-    java.util.concurrent.ScheduledExecutorService pool;
+    private java.util.concurrent.ScheduledExecutorService pool;
 
     public DumpInfo() {
-        int initDelay = 5;
+        int initDelay = 0;
         int delay = 0;
         try {
-            delay = Integer.parseInt(System.getProperty("periodicallyDumpInfo", "0"));
+            delay = Integer.parseInt(System.getProperty("dumpInfo.delay", "0"));
+            delay = Integer.parseInt(System.getProperty("dumpInfo.init", "0"));
         } catch (NumberFormatException e) {
         }
-        
+
         if (delay > 0) {
             pool = Executors.newScheduledThreadPool(1);
             ThreadMXBean threadMXBean = java.lang.management.ManagementFactory.getThreadMXBean();
@@ -48,9 +52,9 @@ public class DumpInfo implements AutoCloseable {
                 MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
                 MemoryUsage nonHeapMemoryUsage = memoryMXBean.getNonHeapMemoryUsage();
                 System.err.printf(""
-                        + "Heap memory usage:    init = %9s, used = %9s, commited = %9s, max = %s\n"
-                        + "NonHeap memory usage: init = %9s, used = %9s, commited = %9s, max = %s\n"
-                        + "Object pending finalization: %d\n\n",
+                                          + "Heap memory usage:    init = %9s, used = %9s, commited = %9s, max = %s\n"
+                                          + "NonHeap memory usage: init = %9s, used = %9s, commited = %9s, max = %s\n"
+                                          + "Object pending finalization: %d\n\n",
                         memoryToHuman.apply(heapMemoryUsage.getInit()),
                         memoryToHuman.apply(heapMemoryUsage.getUsed()),
                         memoryToHuman.apply(heapMemoryUsage.getCommitted()),
@@ -64,16 +68,85 @@ public class DumpInfo implements AutoCloseable {
 
                 java.util.Arrays
                         .stream(threadMXBean.dumpAllThreads(objectMonitorUsage, synchronizerUsage))
-                        .forEach(System.err::print);
+                        .sorted(Comparator.comparing(ThreadInfo::getThreadName))
+                        .filter(threadInfo -> threadInfo.getThreadName().matches("^PcjThread-[0-9]*$"))
+                        .map(DumpInfo::printThreadInfo)
+                        .forEach(System.err::println);
+
             }, initDelay, delay, java.util.concurrent.TimeUnit.SECONDS);
         }
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         if (pool != null) {
             pool.shutdownNow();
         }
     }
 
+    private static String printThreadInfo(ThreadInfo threadInfo) {
+        StringBuilder sb = new StringBuilder("\"" + threadInfo.getThreadName() + "\"" +
+//                                                     (threadInfo.isDaemon() ? " daemon" : "") +
+//                                                     " prio=" + threadInfo.getPriority() +
+                                                     " Id=" + threadInfo.getThreadId() + " " +
+                                                     threadInfo.getThreadState());
+        if (threadInfo.getLockName() != null) {
+            sb.append(" on " + threadInfo.getLockName());
+        }
+        if (threadInfo.getLockOwnerName() != null) {
+            sb.append(" owned by \"" + threadInfo.getLockOwnerName() +
+                              "\" Id=" + threadInfo.getLockOwnerId());
+        }
+        if (threadInfo.isSuspended()) {
+            sb.append(" (suspended)");
+        }
+        if (threadInfo.isInNative()) {
+            sb.append(" (in native)");
+        }
+        sb.append('\n');
+        int i = 0;
+        StackTraceElement[] stackTrace = threadInfo.getStackTrace();
+        for (; i < stackTrace.length; i++) {
+            StackTraceElement ste = stackTrace[i];
+            sb.append("\tat " + ste.toString());
+            sb.append('\n');
+            if (i == 0 && threadInfo.getLockInfo() != null) {
+                Thread.State ts = threadInfo.getThreadState();
+                switch (ts) {
+                    case BLOCKED:
+                        sb.append("\t-  blocked on " + threadInfo.getLockInfo());
+                        sb.append('\n');
+                        break;
+                    case WAITING:
+                        sb.append("\t-  waiting on " + threadInfo.getLockInfo());
+                        sb.append('\n');
+                        break;
+                    case TIMED_WAITING:
+                        sb.append("\t-  waiting on " + threadInfo.getLockInfo());
+                        sb.append('\n');
+                        break;
+                    default:
+                }
+            }
+
+            for (MonitorInfo mi : threadInfo.getLockedMonitors()) {
+                if (mi.getLockedStackDepth() == i) {
+                    sb.append("\t-  locked " + mi);
+                    sb.append('\n');
+                }
+            }
+        }
+
+        LockInfo[] locks = threadInfo.getLockedSynchronizers();
+        if (locks.length > 0) {
+            sb.append("\n\tNumber of locked synchronizers = " + locks.length);
+            sb.append('\n');
+            for (LockInfo li : locks) {
+                sb.append("\t- " + li);
+                sb.append('\n');
+            }
+        }
+        sb.append('\n');
+        return sb.toString();
+    }
 }
